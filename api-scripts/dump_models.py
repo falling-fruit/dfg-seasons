3302bb6b444bcc23655755bf3ff0cf8966b05b84
andrewsuh98 <andrewsuh98@gmail.com>
2023-06-15 19:05:58 -0400

    add sample models and save it in pickle file

---
+++ api-scripts/dump_models.py
@@ -0,0 +1,20 @@
+import joblib
+from models import SampleModel
+
+if __name__ == "__main__":
+    # sample_types = ["apple", "banana", "cherry", "durian", "eggplant"]
+
+    model = SampleModel()
+    joblib.dump(model, "./models/apple_model.pkl")
+
+    model = SampleModel()
+    joblib.dump(model, "./models/banana_model.pkl")
+
+    model = SampleModel()
+    joblib.dump(model, "./models/cherry_model.pkl")
+
+    model = SampleModel()
+    joblib.dump(model, "./models/durian_model.pkl")
+
+    model = SampleModel()
+    joblib.dump(model, "./models/eggplant_model.pkl")
---
+++ api-scripts/models.py
@@ -0,0 +1,10 @@
+from datetime import date
+import random
+
+
+class SampleModel:
+    def __init__(self):
+        pass
+
+    def predict(self, location: str, date: date):
+        return random.random()
