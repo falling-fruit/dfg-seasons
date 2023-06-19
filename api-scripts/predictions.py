d8ec18ba0cd169db6f10c1dbf38cae133a98515c
andrewsuh98 <andrewsuh98@gmail.com>
2023-06-19 13:24:37 -0400

    use latitude longitude as location for predict() function

--- api-scripts/predictions.py
+++ api-scripts/predictions.py
@@ -1,50 +1,35 @@
 """
 This file contains the predict function which takes in a type, location, and date
 and returns a prediction value.
-
-The predict function uses the geopy library to geocode the location and then uses
-the model to make a prediction.
 """
 
 from datetime import date
-from geopy.geocoders import Nominatim
 import joblib
 
 
-# Using Nominatim Api
-geolocator = Nominatim(user_agent="fallingfruit")
-
-
-def predict(type: str, location: str, date: date):
+def predict(type: str, location: tuple[float, float], date: date):
     """
     This function takes in a type, location, and date and returns a prediction value.
     """
 
-    # check if date is in the past
-    if date < date.today():
+    # check if date is in the future
+    if date <= date.today():
         raise Exception("Invalid date. Date is in the past.")
-
-    # Using geocode()
-    geocode_location = geolocator.geocode(location)
-
-    # check if location is valid
-    if not geocode_location:
-        raise Exception("Invalid location")
 
     # Loading model
     try:
         type = type.lower()
         model = joblib.load(f"./models/{type}_model.pkl")
-        prediction = model.predict(geocode_location, date)
+        prediction = model.predict(location, date)
     except FileNotFoundError:
         raise Exception("Invalid type")
 
     # Displaying result details
     print(f"Type: {type}")
-    print(f"Location: {geocode_location}")
+    print(f"Latitude Longitude: {location}")
     print(f"Prediction Value: {prediction}")
     return prediction
 
 
 if __name__ == "__main__":
-    predict("apple", "20500 USA", date(2024, 1, 1))
+    predict("apple", (40.712776, -74.005974), date(2024, 1, 1))
