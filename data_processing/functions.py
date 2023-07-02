e177d0e4a86859984bf342d0d595a3ba7ce73102
peiciqiu <peiciqiu@seas.upenn.edu>
2023-07-02 11:42:41 +0800

    added a function to find unique loc ids and their lat lng

--- data_processing/functions.py
+++ data_processing/functions.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def load_data(csv):
-    df = pd.read_csv('temp.csv')
+    df = pd.read_csv(csv)
     return df
 
 
@@ -12,4 +12,16 @@ def filter_by_long_lat(df, long, lat, delta_long, delta_lat):
     
     ##return the desired rows(regions) in pandas dataframe 
     return df2
+
+def find_unique_location_ids(df):
+
+    # take three columns: location id, lat, long
+    df = df[['location_id', 'lat', 'lng']]
+    # filter by unique location id 
+    df = df.drop_duplicates(subset=['location_id'])
+    # use the first seen lat long for duplicate loc id
+    return df
+
+
+
 
